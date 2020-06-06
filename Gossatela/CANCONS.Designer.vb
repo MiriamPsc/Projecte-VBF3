11c6fe475df2c6157941bcee9e19ff82d8ae7c97
miriaampriscilaa@hotmail.com <miriaampriscilaa@hotmail.com>
2020-06-05 19:58:31 -0500

    PERDONA (AHORA SÍ QUE SÍ)

---
+++ Gossatela/CANCONS.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CANCONS
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btnVolver2 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Itty Bitty Notebook", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(522, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(126, 39)
+        Me.Label1.TabIndex = 49
+        Me.Label1.Text = "CANÇONS"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RichTextBox3)
+        Me.GroupBox2.Controls.Add(Me.Button6)
+        Me.GroupBox2.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(838, 50)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(410, 519)
+        Me.GroupBox2.TabIndex = 48
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Cabrona de Mi"
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.Font = New System.Drawing.Font("Arial Narrow", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox3.Location = New System.Drawing.Point(6, 76)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.Size = New System.Drawing.Size(398, 437)
+        Me.RichTextBox3.TabIndex = 34
+        Me.RichTextBox3.Text = ""
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Gold
+        Me.Button6.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(6, 31)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(58, 39)
+        Me.Button6.TabIndex = 32
+        Me.Button6.Text = "PLAY"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Gold
+        Me.Button2.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(997, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(64, 39)
+        Me.Button2.TabIndex = 46
+        Me.Button2.Text = "STOP"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RichTextBox2)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(429, 50)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(410, 519)
+        Me.GroupBox1.TabIndex = 47
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Escriurem"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Font = New System.Drawing.Font("Arial Narrow", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox2.Location = New System.Drawing.Point(6, 76)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(398, 437)
+        Me.RichTextBox2.TabIndex = 34
+        Me.RichTextBox2.Text = ""
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Gold
+        Me.Button4.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(6, 31)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(58, 39)
+        Me.Button4.TabIndex = 32
+        Me.Button4.Text = "PLAY"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'btnVolver2
+        '
+        Me.btnVolver2.BackColor = System.Drawing.Color.Gold
+        Me.btnVolver2.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVolver2.Location = New System.Drawing.Point(11, 5)
+        Me.btnVolver2.Name = "btnVolver2"
+        Me.btnVolver2.Size = New System.Drawing.Size(118, 39)
+        Me.btnVolver2.TabIndex = 45
+        Me.btnVolver2.Text = "TORNAR"
+        Me.btnVolver2.UseVisualStyleBackColor = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.RichTextBox1)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(11, 50)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(410, 519)
+        Me.GroupBox3.TabIndex = 44
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Et Vull Veure"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Arial Narrow", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(6, 76)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(398, 437)
+        Me.RichTextBox1.TabIndex = 34
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Gold
+        Me.Button1.Font = New System.Drawing.Font("Itty Bitty Notebook", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(6, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(58, 39)
+        Me.Button1.TabIndex = 32
+        Me.Button1.Text = "PLAY"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'CANCONS
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1256, 573)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnVolver2)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Name = "CANCONS"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CANCONS"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents btnVolver2 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Gossatela/CANCONS.vb
@@ -0,0 +1,49 @@
+﻿Public Class CANCONS
+    Dim Archivo As String
+    Dim Carpeta As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Computer.Audio.Play("C:\Users\miria\Downloads\alfred-garcia-canta-et-vull-veure.wav", AudioPlayMode.Background)
+        Carpeta = ("C:\Users\miria\Desktop\EtVullVeure.txt")
+        Archivo = My.Computer.FileSystem.ReadAllText(Carpeta)
+        RichTextBox1.Text = Archivo
+        Button1.BackColor = Color.Red
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        My.Computer.Audio.Play("C:\Users\miria\Downloads\miki-nunez-escriurem-lyric-video.wav", AudioPlayMode.Background)
+        Carpeta = ("C:\Users\miria\Desktop\Escriurem.txt")
+        Archivo = My.Computer.FileSystem.ReadAllText(Carpeta)
+        RichTextBox2.Text = Archivo
+        Button4.BackColor = Color.Red
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        My.Computer.Audio.Play("C:\Users\miria\Downloads\cactus-07-cabrona-de-mi.wav", AudioPlayMode.Background)
+        Carpeta = ("C:\Users\miria\Desktop\CabronaDeMi.txt")
+        Archivo = My.Computer.FileSystem.ReadAllText(Carpeta)
+        RichTextBox3.Text = Archivo
+        Button6.BackColor = Color.Red
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        My.Computer.Audio.Stop()
+
+        Button1.BackColor = Color.Gold
+        Button4.BackColor = Color.Gold
+        Button6.BackColor = Color.Gold
+
+        RichTextBox1.Text = ("")
+        RichTextBox2.Text = ("")
+        RichTextBox3.Text = ("")
+
+    End Sub
+
+    Private Sub Canciones_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Dim S As New Salir()
+        S.Salida()
+    End Sub
+
+    Private Sub BtnVolver2_Click(sender As Object, e As EventArgs) Handles btnVolver2.Click
+        Form1.Show()
+        Hide()
+    End Sub
+End Class
